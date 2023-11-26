9d7895fda154d58b0fb4ff066fc2ca1e65fb2c6e
C. Moid <moid@cmoid.org>
2023-11-26 16:41:54 -0500

    start config before keys

--- apps/ssb/src/ssb_sup.erl
+++ apps/ssb/src/ssb_sup.erl
@@ -25,6 +25,6 @@ start_link() ->
 init([]) ->
     {ok, { {one_for_one, 5, 10},
            [?CHILD(pulse, worker),
-            ?CHILD(keys, worker),
             ?CHILD(config, worker),
+            ?CHILD(keys, worker),
             ?CHILD(mess_auth, worker)]}}.
