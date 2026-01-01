aa223cf904e18e778b1fb4ea43d5a3108589eb78
C. Moid <moid@cmoid.org>
2026-01-01 17:16:50 -0500

    add ebt function stubs

--- apps/ssb/src/ebt.erl
+++ apps/ssb/src/ebt.erl
@@ -6,7 +6,8 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/0,
+         initial_vector/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -24,6 +25,9 @@
           ignore.
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+initial_vector() ->
+    nop.
 
 -doc "Initializes the server.".
 
