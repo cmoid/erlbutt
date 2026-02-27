ccc8607b26688975520c00e916a9edf01faefd5d
C. Moid <moid@cmoid.org>
2026-02-27 15:54:41 -0500

    clean up template specs

--- apps/ssb/src/ebt.erl
+++ apps/ssb/src/ebt.erl
@@ -17,12 +17,7 @@
 
 -record(state, {}).
 
--doc "Starts the server.".
 
--spec start_link() -> {ok, Pid :: pid()} |
-          {error, Error :: {already_started, pid()}} |
-          {error, Error :: term()} |
-          ignore.
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
@@ -32,77 +27,23 @@ initial_vector() ->
     PeerKey = keys:pub_key_disp(),
     {[{PeerKey, ebt_vc:encode_clock_int(true, true, 0)}]}.
 
--doc "Initializes the server.".
 
--spec init(Args :: term()) -> {ok, State :: term()} |
-          {ok, State :: term(), Timeout :: timeout()} |
-          {ok, State :: term(), hibernate} |
-          {stop, Reason :: term()} |
-          ignore.
 init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
--doc """
-Handling call messages
-""".
-
--spec handle_call(Request :: term(), From :: {pid(), term()}, State :: term()) ->
-          {reply, Reply :: term(), NewState :: term()} |
-          {reply, Reply :: term(), NewState :: term(), Timeout :: timeout()} |
-          {reply, Reply :: term(), NewState :: term(), hibernate} |
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-          {noreply, NewState :: term(), hibernate} |
-          {stop, Reason :: term(), Reply :: term(), NewState :: term()} |
-          {stop, Reason :: term(), NewState :: term()}.
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
--doc """
-Handling cast messages
-""".
-
--spec handle_cast(Request :: term(), State :: term()) ->
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-          {noreply, NewState :: term(), hibernate} |
-          {stop, Reason :: term(), NewState :: term()}.
 handle_cast(_Request, State) ->
     {noreply, State}.
 
--doc """
-Handling all non call/cast messages
-""".
-
--spec handle_info(Info :: timeout() | term(), State :: term()) ->
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-          {noreply, NewState :: term(), hibernate} |
-          {stop, Reason :: normal | term(), NewState :: term()}.
 handle_info(_Info, State) ->
     {noreply, State}.
 
--doc """
-This function is called by a gen_server when it is about to
-terminate. It should be the opposite of Module:init/1 and do any
-necessary cleaning up. When it returns, the gen_server terminates
-with Reason. The return value is ignored.
-""".
-
--spec terminate(Reason :: normal | shutdown | {shutdown, term()} | term(),
-                State :: term()) -> any().
 terminate(_Reason, _State) ->
     ok.
 
--doc """
-Convert process state when code is changed
-""".
-
--spec code_change(OldVsn :: term() | {down, term()},
-                  State :: term(),
-                  Extra :: term()) -> {ok, NewState :: term()} |
-          {error, Reason :: term()}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
