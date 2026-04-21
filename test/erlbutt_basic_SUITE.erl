286bafec5032b0259ec5b11c3147d4c48b5a42fb
C. Moid <moid@cmoid.org>
2026-04-21 19:43:51 -0400

    clean up ct basic_test

--- test/erlbutt_basic_SUITE.erl
+++ test/erlbutt_basic_SUITE.erl
@@ -28,6 +28,7 @@ init_per_suite(_Config) ->
     _Config.
 
 end_per_suite(_Config) ->
+    application:stop(ssb),
     _Config.
 
 init_per_testcase(_Case, _Config) ->
@@ -37,10 +38,7 @@ end_per_testcase(_, Config) ->
     Config.
 
 all() ->
-    [connect_test
-     %%ping_test,
-     %%whoami_test
-    ].
+    [connect_test].
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,39 +51,5 @@ connect_test(Config) ->
     Config.
 
 
-ping_test(Config) ->
-    Host = get_value(hostname, Config, "localhost"),
-    {ok, NewSbotPeer} = ssb_peer:start_link(Host, remote_long_pk()),
-    Now = erlang:system_time(millisecond),
-    %% This send call is no longer synchronous but something like it needs to exist
-    Time = ssb_peer:send(NewSbotPeer, ping()),
-    End = binary_to_integer(message:nat_decode(Time)),
-    ?assert((End - Now) < 5),
-    Config.
-
-whoami_test(Config) ->
-    Host = get_value(hostname, Config, "localhost"),
-    {ok, NewSbotClient} = ssb_peer:start_link(Host, remote_long_pk()),
-    {WhoAmI} = utils:nat_decode(ssb_peer:send(NewSbotClient, whoami_req())),
-    ?assert(keys:pub_key_disp() == ?pgv(<<"id">>, WhoAmI)),
-    Config.
-
-
 remote_long_pk() ->
     base64:decode(keys:pub_key()).
-
-ping() ->
-    Flags = rpc_processor:create_flags(1,0,2),
-    Body = utils:encode_rec({[{<<"name">>,[<<"gossip">>,<<"ping">>]},
-                          {<<"args">>,[{[{<<"timeout">>, 300000}]}]},
-                          {<<"type">>,<<"duplex">>}]}),
-    Header = rpc_processor:create_header(Flags, size(Body), 1),
-    utils:combine(Header, Body).
-
-whoami_req() ->
-    Flags = rpc_processor:create_flags(1,0,2),
-    Body = iolist_to_binary(message:ssb_encoder({[{<<"name">>,[?whoami]},
-                          {<<"args">>,[]},
-                          {<<"type">>,<<"async">>}]}, fun message:ssb_encoder/3, [])),
-    Header = rpc_processor:create_header(Flags, size(Body), 1),
-    utils:combine(Header, Body).
