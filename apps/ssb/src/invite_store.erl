a9a6593c1a7461400e056eddd96102010cd2bdfb
C. Moid <moid@cmoid.org>
2026-05-19 14:14:01 -0400

    remove unused include

--- apps/ssb/src/invite_store.erl
+++ apps/ssb/src/invite_store.erl
@@ -9,8 +9,6 @@
 
 -behaviour(gen_server).
 
--include_lib("ssb/include/ssb.hrl").
-
 -export([start_link/0, store/1, is_invite/1, validate_and_consume/1, list/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
