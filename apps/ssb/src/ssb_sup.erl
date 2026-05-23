3ea9d350bf6377e56191250c400547acea39f1b7
C. Moid <moid@cmoid.org>
2026-05-23 16:29:51 -0400

    add LRU network id cache to supervisor

--- apps/ssb/src/ssb_sup.erl
+++ apps/ssb/src/ssb_sup.erl
@@ -25,6 +25,7 @@ start_link() ->
 init([]) ->
     {ok, { {one_for_one, 5, 10},
            [?CHILD(config, worker),
+            ?CHILD(network_id_cache, worker),
             ?CHILD(heartbeat, worker),
             ?CHILD(keys, worker),
             ?CHILD(blobs, worker),
